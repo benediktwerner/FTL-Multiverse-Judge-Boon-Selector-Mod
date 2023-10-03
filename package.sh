63f15b676927c7bbdcb1f8fdc27e79523f6ab57d
Benedikt Werner <1benediktwerner@gmail.com>
2023-10-03 21:17:05 +0200

    Initial commit

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm "Multiverse Judge Boon Selector.zip"
+rm "Multiverse Judge Boon Selector.ftl"
+zip -r "Multiverse Judge Boon Selector.zip" data mod-appendix -x \*.DS_Store -x __MACOSX/\*
+cp "Multiverse Judge Boon Selector.zip" "/Users/benedikt.werner/Applications/Steambuild 64bit D3d11.app/Contents/SharedSupport/prefix/drive_c/Program Files (x86)/Steam/steamapps/common/SlipstreamModManager_1.9.1-Win/mods/Multiverse Judge Boon Selector.zip"
+mv "Multiverse Judge Boon Selector.zip" "Multiverse Judge Boon Selector.ftl"
